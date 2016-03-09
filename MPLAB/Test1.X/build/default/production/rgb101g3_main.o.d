b5a6a4dbd063d9f8610c8f6e97d1cf2d13d98e51
htvdanhgithub <htvdanh@gmail.com>
2016-03-10 01:43:35 +0700

    Delete MPLAB folder

--- MPLAB/Test1.X/build/default/production/rgb101g3_main.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/rgb101g3_main.o:  \
-	D:/Working/MPLAB/Test1.X/rgb101g3_main.asm  \
-	D:/Working/MPLAB/Test1.X/rgb101g3_start.inc  \
-	D:/Working/MPLAB/Test1.X/rgb101g3_func.inc  \
-	D:/Working/MPLAB/Test1.X/rgb101g3_tout.inc  \
-	D:/Working/MPLAB/Test1.X/SequenceData.inc  \
-
