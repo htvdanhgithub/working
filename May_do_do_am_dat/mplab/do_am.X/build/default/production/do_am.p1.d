085f80f89bed65393cdf2cf611ea3dabb85e266f
htvdanhgithub <htvdanh@gmail.com>
2016-05-16 22:38:03 +0700

    Add do_am project

---
+++ May_do_do_am_dat/mplab/do_am.X/build/default/production/do_am.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/do_am.d  \
+ build/default/production/do_am.p1:  \
+ do_am.c  \
+lcd_hd44780_pic16.h 
---
+++ danhll/MPLAB/RGB_C/do_am.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=do_am.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/do_am.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=doam.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/doam.x.tar
