dc298bddc9a8b4ba24b0017f034bd1b1ad377994
htvdanhgithub <htvdanh@gmail.com>
2024-03-25 02:42:03 +0700

    Add Format

--- ChatServer/main.py
+++ ChatServer/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
@@ -7,6 +8,9 @@ socketio = SocketIO(app)
 
 # Dictionary to store the connection ID of each client
 clients = {}
+
+# Dictionary to store the selected language of each client
+languages = {}
 
 @app.route('/')
 def index():
@@ -25,14 +29,29 @@ def handle_disconnect():
     del clients[connection_id]
     print(f"Client disconnected with ID: {connection_id}")
 
-@socketio.on('send_message')
-def handle_send_message(data):
+@socketio.on('set_language')
+def handle_set_language(data):
+    connection_id = request.sid
+    language = data['language']
+    languages[connection_id] = language
+    print(f"Language set for client {connection_id}: {language}")
+
+@socketio.on('send_text_message')
+def handle_send_text_message(data):
     sender_id = request.sid
     recipient_id = data['recipient_id']
     message = data['message']
-    print(f"Message sent from {sender_id} to {recipient_id}: {message}")
-    emit('message', {'sender_id': "You", 'message': message}, room=recipient_id)
-#    emit('message', {'sender_id': sender_id, 'message': message}, room=sender_id)  # Echo message back to sender
+    print(f"Text message sent from {sender_id} to {recipient_id}: {message}")
+    emit('text_message', {'sender_id': "You", 'message': message}, room=recipient_id)
+
+@socketio.on('send_image')
+def handle_send_image(data):
+    sender_id = request.sid
+    recipient_id = data['recipient_id']
+    image = data['image']
+    print(f"Image sent from {sender_id} to {recipient_id}: {image}")
+    emit('image', {'sender_id': sender_id, 'image': image}, room=recipient_id)
+    #emit('image', {'sender_id': sender_id, 'image': image}, room=sender_id)  # Echo image back to sender
 
 if __name__ == '__main__':
     socketio.run(app, allow_unsafe_werkzeug=True, debug=True)
