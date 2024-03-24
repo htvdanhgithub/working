ee0e20fa82a65db7ac923fa66f45d73b55ac0e0c
htvdanhgithub <htvdanh@gmail.com>
2024-03-24 23:14:05 +0700

    Add ChatServer

---
+++ ChatServer/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
+
+# Dictionary to store the connection ID of each client
+clients = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    connection_id = request.sid
+    clients[connection_id] = True
+    print(f"Client connected with ID: {connection_id}")
+    emit('connection_id', connection_id)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    connection_id = request.sid
+    del clients[connection_id]
+    print(f"Client disconnected with ID: {connection_id}")
+
+@socketio.on('send_message')
+def handle_send_message(data):
+    sender_id = request.sid
+    recipient_id = data['recipient_id']
+    message = data['message']
+    print(f"Message sent from {sender_id} to {recipient_id}: {message}")
+    emit('message', {'sender_id': "You", 'message': message}, room=recipient_id)
+#    emit('message', {'sender_id': sender_id, 'message': message}, room=sender_id)  # Echo message back to sender
+
+if __name__ == '__main__':
+    socketio.run(app, allow_unsafe_werkzeug=True, debug=True)
